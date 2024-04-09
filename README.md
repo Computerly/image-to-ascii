bd109cf35804c2a926216fd48454d9fccfd1fbae
Caleb <caleb0515@live.com>
2024-04-09 14:06:30 -0500

    Add .gitignore file and implement imageToAscii function in imagetoascii.py

---
+++ README.md
@@ -0,0 +1,5 @@
+# Image to ASCII
+This program transforms an image into an ascii representation of that image.
+
+April 4th, 2024
+Caleb Venkatrathnam
--- imagetoascii.py
+++ imagetoascii.py
@@ -1,14 +1,20 @@
 from PIL import Image
 import numpy as np
 import math
+from collections import Counter
 
 ASCII_CHARS = " .:-=+*|#@" # Inspo Credit: https://paulbourke.net/dataformats/asciiart/
 
-def rgbToGrayScale(rgb):
-	return 0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]
+
+def rgbToGrayScale(r,g,b):
+	try:
+		return 0.299 * r + 0.587 * g + 0.114 * b
+	except:
+		print(r,g,b)
+		raise Exception("Error in converting RGB to Grayscale")
 
 # ASCII art will always be 100 characters wide
-def imageToAscii(input_image_path, width=100):
+def imageToAscii(input_image_path, width=100, invert=False):
 	ascii_image = ""
  
  	# open the image
@@ -23,24 +29,30 @@ def imageToAscii(input_image_path, width=100):
 	if(type.upper() != 'JPEG' and type.upper() != 'JPG'):
 		raise Exception("Image must be in JPEG format")
 	
- # Resize the image
+ 	# Resize the image
 	image.resize((width, math.ceil(width * h / w))).save("resized.%s" % type)
- 
+	
 	# open resized image
 	img = Image.open("resized.%s" % type)
+ 	
+  	# convert to rgb
+	img = img.convert('RGB')
+ 
+	pixels = img.load()
 	
  	# redefine width and height
 	w,h = img.size	
  
- 
-	pixels = img.load()
- 
 	for y in range(h):
 		for x in range(w):
 			# Get grayscale value of pixel
-			gScale = rgbToGrayScale(pixels[x,y])
+			px = pixels[x,y]
+			gScale = rgbToGrayScale(px[0], px[1], px[2])
 			# Get the ascii character
-			ascii_image += ASCII_CHARS[math.floor(gScale * ((len(ASCII_CHARS) - 1) / 255))]
+			if invert:
+				ascii_image += ASCII_CHARS[math.floor((255 - gScale) * ((len(ASCII_CHARS) - 1) / 255))]
+			else:
+				ascii_image += ASCII_CHARS[math.floor(gScale * ((len(ASCII_CHARS) - 1) / 255))]
 		ascii_image += "\n"
 	
 	return ascii_image
@@ -53,8 +65,7 @@ if __name__ == '__main__':
 	output_ascii_path = "ascii.txt"
  	
 	# Convert the image to ascii
-	ascii_image = imageToAscii(image_path, width)
-	# Print the ascii image
+	ascii_image = imageToAscii(image_path, width, True)
 	print(ascii_image)
  	# Save the ascii image
 	with open(output_ascii_path, "w") as f:
